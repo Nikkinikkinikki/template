84509b6d0576f39364715cfba13db9636c6b6c72
Nikkinikkinikki <49133020+Nikkinikkinikki@users.noreply.github.com>
2019-05-15 18:18:53 +1000

    Update waf.py

--- template/waf.py
+++ template/waf.py
@@ -19,12 +19,6 @@ def detect_attack(string_in):
         return 'True'
     return 'False'
 
-@post('/waf/email/<email:path>')
-def verify_email(email):
-    if '@' in email:
-        return 'True'
-    else:
-        return "Not an email address"
 
 @post('/waf/password/<password:path>')
 def verify_password(password):
@@ -57,6 +51,83 @@ def enable_debugger():
         debug = False
     else:
         debug = True
+
+# Run the server
+run(host=host, port=port)
+gininput)
+                return "Stop attacking our website."
+
+
+        print("noattack")
+        return "True"
+    print("debugging")
+    return "False"
+
+@post('/waf/password/<input_from:path>/<password:path>')
+def verify_password(input_from, password):
+    if len(password) < 8:
+        print("Password is too short")
+        return "Password is too short"
+
+    if not any(c in string.ascii_lowercase for c in password):
+        print("Password must contain at least one lowercase character")
+        return "Password must contain at least one lowercase character"
+
+    if not any(c in string.ascii_uppercase for c in password):
+        print("Password must contain at least one uppercase character")
+        return "Password must contain at least one uppercase character"
+
+    inj_str = ['\'', '-', ' ', '<','>','script']
+    for word in inj_str:
+        if word in password:
+            print(word)
+            #report_attack(input_from, word, origininput)
+            return "Don't include \'" + word + "\' in your username or password."
+
+    print("password valid")
+    return 'True'
+
+# droped function
+@post('/waf/originstring/<string_in:path>')
+def get_origin_string(string_in):
+    print("get_origin_string")
+    print(string_in)
+
+    if not debug:
+        inj_str = ['\'','-']
+        for words in inj_str:
+            return "False"
+        return "True"
+    return "False"
+
+
+# Rather than using paths, you could throw all the requests with form data filled using the
+# requests module and extract it here. Alternatively you could use JSON objects.
+
+# Custom definition waf
+@post('/waf/custom/field=<field:path>%20test=<test:path>')
+def custom_waf(field, test):
+    if re.search(test, field) is not None:
+        return "True"
+    return "False"
+
+# Debug toggle
+@post('/waf/debug')
+def enable_debugger():
+    global debug
+    if debug:
+        debug = False
+    else:
+        debug = True
+
+
+
+if len(sys.argv) == 4:
+    host = sys.argv[1]
+    port = sys.argv[2]
+    reprot_file = sys.argv[3]
+
+
 
 # Run the server
 run(host=host, port=port)
